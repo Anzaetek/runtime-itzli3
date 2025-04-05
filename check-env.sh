a6728375f85dcde1a3fa824c5457cfaa7a32a2da
R B <rb@laplace.kr>
2025-04-05 17:22:07 +0900

    env

---
+++ check-env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+SCRIPTPATH=`dirname "$(readlink -f "$0")"`
+cd $SCRIPTPATH 
+
+if [ -d Itliz3 ]; then
+    echo Itzli3 present
+else
+    cat itzli3-{a,b,c,d,e}* > Itzli3.tar.bz2
+    tar jxvf Itzli3.tar.bz2
+fi 
---
+++ clean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+SCRIPTPATH=`dirname "$(readlink -f "$0")"`
+cd $SCRIPTPATH 
+
+rm -Rvf Itzli3 Itzli3.tar.bz2 
